14c408ec1bdb84fe2d83f61d8e7c9de3ead5b401
peter-426 <phebden@gmail.com>
2022-02-20 19:43:40 +0000

    x

---
+++ code-for-r-pi/MF-Camera/python/Motorized-Focus-Camera-3-picker.py
@@ -0,0 +1,332 @@
+
+import picamera
+import os
+import time
+import sys
+from datetime import datetime, timedelta
+from threading import Thread
+from ctypes import *
+keyboard = CDLL('./lib/libarducam_keyboard.so')
+arducam_vcm =CDLL('./lib/libarducam_vcm.so')
+
+import Autofocus as af
+#import classify_picamera as cp
+import object_detection_tflite as od
+
+
+class Cam:
+    saved_img_filename=""
+    min_conf = 0.10
+    max_boxes = 2   # max box count, max number of colonies to detect
+            
+    # location cases 
+    location_5_holder = ord('f'); # colony plate on flat holder at desk loc 5
+    
+    up   =  1;     down  =  2
+    left = 68;     right = 67   
+    larger = ord('+'); smaller = ord('-')
+    
+    focus_in = ord('i'); focus_out = ord('o')
+    
+    SAVE = ord('s')
+    
+    focus_val = 512; step = 10
+    vflip = ord('v');    hflip = ord('h')
+    
+    x=10; y=20; width=412; height=412;
+    
+    # zx was 0.5,  made smaller to shift initial window position left
+    zx=0.30; zy=0.480; zw=0.440; zh=0.440;
+    
+    expand=ord('e');  contract=ord('c')
+    
+    inc_brightness=ord('1'); dec_brightness=ord('2');
+    inc_contrast  =ord('3'); dec_contrast  =ord('4');
+    inc_saturation=ord('5'); dec_saturation=ord('6');
+    inc_sharpness =ord('6'); dec_sharpness =ord('7');
+    
+    # default resolution: is bigger than 1600x1600 
+    res_width=1600;  res_height=1600;  res_step=10;
+    more_res=ord('m');  less_res=ord('l')
+    
+    camera = picamera.PiCamera()
+    
+    def menu(self):
+        print("Press up, down, left, right keys to move window\n")
+        
+        print('f set window for location 5, plate on holder\n')
+        
+        print('+ inc window size,  - dec window size')
+        print('i focus in,         o focus out')
+        print('v vertical flip,    h horizontal flip')
+        print('e expand,           c contract\n')
+        
+        print('1 increase brightness,  2 decrease brightness')
+        print('3 increase contrast,    4 decrease contrast')
+        print('5 increase saturation,  6 decrease saturation')        
+        print('7 increase sharpness,   8 decrease sharpness\n')
+        
+        print('m more resolution, l less resolution\n')
+        
+        print('a autofocus')
+        print('r object detection on saved image')
+        print('s save pic as img_ + time-stamp')
+        print('q quit\n')            
+    def __init__(self):
+        self._running = True
+        self.camera.resolution = (self.res_width, self.res_height)  # res set here!!!
+        
+        
+    def terminate(self):  
+        self._running = False  
+
+    def run(self):
+        #os.system("raspistill -t 0")     
+        self.camera.start_preview(fullscreen=False, window=(self.x, self.y, self.width, self.height))
+        self.camera.zoom=(self.zx, self.zy, self.zw, self.zh)
+        #self.camera.zoom=(self.zx, self.zy, self.zw, self.zh)
+        arducam_vcm.vcm_write(self.focus_val)
+        
+    def close(self):
+        self.camera.close()
+       
+    def save_pic(self, filename):
+        try:
+          self.camera.capture(filename)
+          self.saved_img_filename=filename
+        finally:
+          pass
+
+
+    def update(self, arducam_vcm, keyVal):
+        # pan
+        delta=0.01
+        
+        if keyVal == self.location_5_holder:
+            self.zx=0.300; self.zy=0.480; self.zw=0.440; self.zh=0.440;
+            self.camera.zoom=(self.zx, self.zy, self.zw, self.zh)
+            print("f: loc 5, window position (x,y,w,h): %0.3f,%0.3f %0.3f,%0.3f" %(self.zx, self.zy, self.zw, self.zh))
+                
+        elif keyVal == self.up:
+            self.zy -= delta
+            if self.zy < 0:
+                self.zy = 0
+            self.camera.zoom=(self.zx, self.zy, self.zw, self.zh)
+            print("up:   window position (x,y,w,h): %0.3f,%0.3f %0.3f,%0.3f" %(self.zx, self.zy, self.zw, self.zh))
+            
+        elif keyVal == self.down:         
+            if self.zy + delta + self.zh <= 1:
+                self.zy += delta
+            self.camera.zoom=(self.zx, self.zy, self.zw, self.zh)
+            print("down: window position (x,y,w,h): %0.3f,%0.3f %0.3f,%0.3f" %(self.zx, self.zy, self.zw, self.zh))
+            
+        elif keyVal == self.left:
+            self.zx -= delta
+            if self.zx < 0:
+                self.zx = 0
+            self.camera.zoom=(self.zx, self.zy, self.zw, self.zh)
+            print("<--: window position (x,y,w,h): %0.3f,%0.3f %0.3f,%0.3f" %(self.zx, self.zy, self.zw, self.zh))
+            
+        elif keyVal == self.right:        
+            if self.zx + delta + self.zw <= 1:
+                self.zx += delta; 
+            #print(self.zx, self.zw)
+            self.camera.zoom=(self.zx, self.zy, self.zw, self.zh)
+            print("-->: window position (x,y,w,h): %0.3f,%0.3f %0.3f,%0.3f" %(self.zx, self.zy, self.zw, self.zh))
+            
+        elif keyVal == self.expand:
+            self.zx=0; self.zy=0; self.zw=1; self.zh=1;           
+            self.camera.zoom=(0,0,1,1)           
+            print("e: window position,zoom out (x,y,w,h): %0.3f,%0.3f %0.3f,%0.3f" %(self.zx, self.zy, self.zw, self.zh))
+           
+        elif keyVal == self.contract:      
+            if self.zw - delta*2 >= 0.10 and self.zh - delta*2 >= 0.10:
+                self.zw -= delta*2
+                self.zh -= delta*2
+                self.zx += delta;
+                self.zy += delta
+                self.camera.zoom=(self.zx, self.zy, self.zw, self.zh)
+            print("c: window position,zoom in  (x,y,w,h): %0.3f,%0.3f %0.3f,%0.3f" %(self.zx, self.zy, self.zw, self.zh))
+            
+        elif keyVal == self.larger:
+            self.width  += 50
+            self.height += 50
+            self.camera.preview_window=(self.x, self.y, self.width, self.height)
+            print("+: window size: %dx%d" %(self.width,self.height))
+            
+        elif keyVal == self.smaller:
+            self.width  -= 50
+            self.height -= 50
+            self.camera.preview_window=(self.x, self.y, self.width, self.height)
+            print("-: window size: %dx%d" %(self.width,self.height))
+            
+        elif keyVal == self.focus_in:
+            self.focus_val = self.focus_val+self.step
+            if  self.focus_val > 1023:
+                self.focus_val = 1023
+            arducam_vcm.vcm_write(self.focus_val)
+            
+        elif keyVal == self.focus_out:
+            self.focus_val = self.focus_val - self.step
+            if self.focus_val < 0:
+                self.focus_val = 0
+            arducam_vcm.vcm_write(self.focus_val)
+            
+        elif keyVal == self.vflip:
+            if self.camera.vflip == False:
+                self.camera.vflip=True
+            else:
+                self.camera.vflip=False 
+        elif keyVal == self.hflip:
+            if self.camera.hflip == False:
+                self.camera.hflip=True
+            else:
+                self.camera.hflip=False     
+        elif keyVal == self.more_res:
+            self.res_width  += self.res_step
+            self.res_height += self.res_step
+            self.camera.resolution = (self.res_width, self.res_height)
+            print("m: resolution: %sx%s" %(self.res_width,self.res_height))
+            
+        elif keyVal == self.inc_brightness:
+            self.camera.brightness += 1
+            print('1: camera.brightness: ', self.camera.brightness)
+            
+        elif keyVal == self.dec_brightness:
+            self.camera.brightness -= 1
+            print('2: camera.brightness: ', self.camera.brightness)
+            
+        elif keyVal == self.inc_contrast:
+            self.camera.contrast += 1
+            print('3: camera.contrast: ', self.camera.contrast)
+            
+        elif keyVal == self.dec_contrast:
+            self.camera.contrast -= 1
+            print('4: camera.contrast: ', self.camera.contrast)
+
+        elif keyVal == self.inc_saturation:
+            self.camera.saturation += 1
+            print('5: camera.saturation: ', self.camera.saturation)
+            
+        elif keyVal == self.dec_saturation:
+            self.camera.saturation -= 1
+            print('6: camera.saturation: ', self.camera.saturation)
+ 
+        elif keyVal == self.inc_sharpness:
+            self.camera.sharpness += 1
+            print('5: camera.sharpness: ', self.camera.sharpness)
+            
+        elif keyVal == self.dec_sharpness:
+            self.camera.sharpness -= 1
+            print('6: camera.sharpness: ', self.camera.sharpness)
+ 
+        elif keyVal == self.less_res:
+            self.res_width  -= self.res_step
+            self.res_height -= self.res_step
+            self.camera.resolution = (self.res_width, self.res_height)
+            print("l: resolution: %dx%d" %(self.res_width,self.res_height))
+            
+        return self.focus_val
+    
+    def auto_focus(self):
+        self.camera.shutter_speed=30000
+        print("Start focusing")
+        
+        max_index = 10
+        max_value = 0.0
+        last_value = 0.0
+        dec_count = 0
+        focal_distance = 10
+
+        while True:
+            #Adjust focus, then calculate image clarity
+            af.focusing(focal_distance)
+            val = af.calculation(self.camera)
+            #Find the maximum image clarity
+            if val > max_value:
+                max_index = focal_distance
+                max_value = val
+                
+            #If the image clarity starts to decrease
+            if val < last_value:
+                dec_count += 1
+            else:
+                dec_count = 0
+            #Image clarity is reduced by six consecutive frames
+            if dec_count > 6:
+                break
+            last_value = val
+            
+            #Increase the focal distance
+            focal_distance += 10
+            if focal_distance > 1000:
+                break
+
+        #Adjust focus to the best
+        af.focusing(max_index)
+        print("Done focusing")
+        return max_index
+
+
+myCam = Cam()
+myCamThread = Thread(target=myCam.run) 
+myCamThread.start()
+
+#Thread.start(run_camera) #, ("run_camera",))
+#vcm init
+arducam_vcm.vcm_init()
+# arducam_vcm.vcm_write(myCam.focus_val)
+# camera = picamera.PiCamera()
+# camera.resolution = (640, 640)
+# camera.start_preview(fullscreen=False, window=(100,100,640,640))
+folder_name = 'images/'
+try:
+    print("Press ? for camera menu.")
+    Quit=ord('q')
+    focus=0
+    while True:
+        print(">> ", end="",  flush=True)
+        
+        keyVal = keyboard.processKeyEvent()     
+        print( chr(keyVal) )
+        
+        if keyVal == Quit:
+            break
+        #time.sleep(0.01)
+        if keyVal == ord('?'):
+            myCam.menu()
+        elif keyVal == ord('a'):
+            focus_val = myCam.auto_focus()
+            myCam.focus_val = focus_val
+        elif keyVal == ord('r'):
+            print(">> ", myCam.saved_img_filename)
+            if os.path.isfile(myCam.saved_img_filename):
+                TEMP_FILE = myCam.saved_img_filename
+            else:
+                TEMP_FILE ='images-test/carotene-01-600-600.jpg'    # just testing
+            
+            print("\nObject Detection: %s,  min_conf %0.2f, max_boxes %d\n" % \
+                  (TEMP_FILE, myCam.min_conf, myCam.max_boxes))
+            
+
+            od.run_od(TEMP_FILE, myCam.min_conf, myCam.max_boxes, keyboard)
+            
+            
+        elif keyVal == ord('s'):
+            print("Current folder: ", folder_name) 
+            
+            image_set_number = input("Enter image set number, or hit Enter for no change: ")
+            if image_set_number.isnumeric():
+                folder_name = "images-" + image_set_number + "/"
+            if not os.path.exists(folder_name):
+                os.makedirs(folder_name)
+                
+            filename= folder_name + "img_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".jpg"
+            print("saved to : ", filename)
+            myCam.save_pic(filename)
+        else:
+            #print(keyVal)
+            focus_val = myCam.update(arducam_vcm, keyVal)
+
+finally:
+    myCam.close()
---
+++ code-for-r-pi/MF-Camera/python/object_detection_tflite.py
@@ -0,0 +1,332 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import numpy as np
+
+from tflite_runtime.interpreter import Interpreter
+
+# The EfficientDet-Lite0 model. EfficientDet-Lite[0-4] are a 
+# family of mobile/IoT-friendly object detection models derived from the 
+# [EfficientDet](https://arxiv.org/abs/1911.09070) architecture.
+# 
+# Here is the performance of each EfficientDet-Lite models compared to each others.
+# 
+# | Model architecture | Size(MB)* | Latency(ms)** | Average Precision*** |
+# |--------------------|-----------|---------------|----------------------|
+# | EfficientDet-Lite0 | 4.4       | 37            | 25.69%               |
+# | EfficientDet-Lite1 | 5.8       | 49            | 30.55%               |
+# | EfficientDet-Lite2 | 7.2       | 69            | 33.97%               |
+# | EfficientDet-Lite3 | 11.4      | 116           | 37.70%               |
+# | EfficientDet-Lite4 | 19.9      | 260           | 41.96%               |
+# 
+
+
+# Load the trained TFLite model and define some visualization functions
+
+import cv2
+
+from PIL import Image
+from numpy import asarray
+
+model_path = 'model.tflite'
+
+# Load the labels into a list
+classes = ['???'] * 1 # model.model_spec.config.num_classes
+label_map = { 0: 'col'} #model.model_spec.config.label_map
+#for label_id, label_name in label_map.as_dict().items():
+#  classes[label_id-1] = label_name
+classes = { 0: 'col'}
+
+# Define a list of colors for visualization
+COLORS = np.random.randint(0, 255, size=(len(classes), 3), dtype=np.uint8)
+
+
+def resize_pad(im, desired_size):
+    ## opencv has copyMakeBorder() method for borders
+    
+    old_size = im.shape[:2] # old_size is in (height, width) format
+    print(old_size)
+    ratio = float(desired_size)/max(old_size)
+    new_size = tuple([int(x*ratio) for x in old_size])
+    
+    # new_size should be in (width, height) format
+    im = cv2.resize(im, (new_size[1], new_size[0])) 
+    
+    delta_w = desired_size - new_size[1]
+    delta_h = desired_size - new_size[0]
+    top, bottom = delta_h//2, delta_h-(delta_h//2)
+    left, right = delta_w//2, delta_w-(delta_w//2)
+    
+    color = [0, 0, 0]
+    new_im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT,
+        value=color)
+    
+    img = cv2.cvtColor(new_im, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(img)
+    return img
+
+def preprocess_image(image_path, input_size):
+  """Preprocess the input image to feed to the TFLite model"""
+#   img = tf.io.read_file(image_path)
+#   img = tf.io.decode_image(img, channels=3)
+#   img = tf.image.convert_image_dtype(img, tf.uint8)
+#   original_image = img
+#   resized_img = tf.image.resize(img, input_size)
+#   resized_img = resized_img[tf.newaxis, :]
+  
+  print("resize to 'input size'", input_size)
+  im = Image.open(image_path)
+  original_image = asarray(im)
+  im=im.resize(input_size)
+  #im = resize_pad(original_image, max(input_size) )
+  return im, original_image
+
+
+def set_input_tensor(interpreter, image):
+  """Set the input tensor."""
+  tensor_index = interpreter.get_input_details()[0]['index']
+  input_tensor = interpreter.tensor(tensor_index)()[0]
+  input_tensor[:, :] = image
+
+
+def get_output_tensor(interpreter, index):
+  """Return the output tensor at the given index."""
+  output_details = interpreter.get_output_details()[index]
+  tensor = np.squeeze(interpreter.get_tensor(output_details['index']))
+  return tensor
+
+
+def boxes_overlap(ymin1, xmin1, ymax1, xmax1, ymin2, xmin2, ymax2, xmax2):
+    
+   # if (xmin1 == xmin2 or ymin1 == ymin2 or xmax1 == xmax2 or ymax1 == ymax2):
+   #    # the line cannot have positive overlap
+   #     return False
+         
+    # If one rectangle is on left side of other
+    if(xmin1 >= xmax2 or xmax1 <= xmin2):
+        return False
+ 
+    # If one rectangle is above other
+    if(ymax1 <= ymin2 or ymax2 <= ymin1):
+        return False
+ 
+    return True
+
+
+# Puts list of results in ascending order by score
+# Returns true if two rectangles overlap
+def filter_results(results):
+
+    results_filtered = []  #{'bounding_box':[], 'score':0, 'class_id': 0 }
+    
+    mylist =[]
+    for ii in range(len(results)):
+        mylist.append(results[ii]['score'])
+
+    mylist=np.array(mylist)
+    idx = np.argsort(mylist*-1)  # want desceding order, == reverse
+
+    results_sorted = []
+    for ii in idx:
+        results_sorted.append(results[ii])
+        
+    results = results_sorted   
+    
+    #verify order    
+#     for ii in range(len(results)):
+#         print(results[ii]['score'])
+    
+    for ii in range( len(results) ):
+        ymin1, xmin1, ymax1, xmax1 = results[ii]['bounding_box']
+        overlap=False
+        
+        for jj in range(len(results) ):
+            if ii==jj:
+                continue
+        
+            ymin2, xmin2, ymax2, xmax2 = results[jj]['bounding_box']
+            
+            if boxes_overlap(ymin1, xmin1, ymax1, xmax1, ymin2, xmin2, ymax2, xmax2) \
+                and results[ii]['score'] <= results[jj]['score']:
+                    overlap=True
+                    break
+            
+        if overlap == False:    
+            results_filtered.append(results[ii])  # for cases of overlap, this box had highest score
+                  
+    return results_filtered
+
+
+
+def detect_objects(interpreter, image, threshold):
+  """Returns a list of detection results, each a dictionary of object info."""
+  # Feed the input image to the model
+  set_input_tensor(interpreter, image)
+  interpreter.invoke()
+
+  # Get all outputs from the model
+  boxes = get_output_tensor(interpreter, 0)
+  classes = get_output_tensor(interpreter, 1)
+  scores = get_output_tensor(interpreter, 2)
+  count = int(get_output_tensor(interpreter, 3))
+
+  results = []
+  for i in range(count):
+    if scores[i] >= threshold:
+      result = {
+        'bounding_box': boxes[i],
+        'class_id': classes[i],
+        'score': scores[i]
+      }
+      results.append(result)
+        
+  #print('first box: ', boxes[0])
+  print('result count: ', len(results))
+    
+  return results
+
+
+
+def run_odt_and_draw_results(image_path, interpreter, filter_boolean, threshold=0.5, max_boxes=10):
+  """Run object detection on the input image and draw the detection results"""
+  # Load the input shape required by the model
+  _, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
+
+  # Load the input image and preprocess it
+  preprocessed_image, original_image = preprocess_image(
+      image_path,
+      (input_height, input_width)
+    )
+
+  # Run object detection on the input image
+  results = detect_objects(interpreter, preprocessed_image, threshold=threshold)
+
+  if filter_boolean:
+      results = filter_results(results)
+      print("number of filtered results: ", len(results))
+
+  # Plot the detection results on the input image
+  original_image_np = original_image # .numpy().astype(np.uint8)
+  #print(type(original_image_np))
+  print(len(original_image_np))
+  
+  counter = 0
+  centers =[]
+  for obj in results:
+    # Convert the object bounding box from relative coordinates to absolute
+    # coordinates based on the original image resolution
+    ymin, xmin, ymax, xmax = obj['bounding_box']
+    xmin = int(xmin * original_image_np.shape[1])
+    xmax = int(xmax * original_image_np.shape[1])
+    ymin = int(ymin * original_image_np.shape[0])
+    ymax = int(ymax * original_image_np.shape[0])
+
+    # Find the class index of the current object
+    class_id = int(obj['class_id'])
+    score = "{}: {:.1f}%".format('col', obj['score'] * 100)
+    
+    print(( int((xmin+xmax)/2),  int((ymin+ymax)/2), classes[class_id]), score )
+    
+    centers.append( ( int((xmin+xmax)/2),  int((ymin+ymax)/2), classes[class_id], score ) )
+
+    # Draw the bounding box and label on the image
+    color = 1 # [int(c) for c in COLORS[class_id]]
+    cv2.rectangle(original_image_np, (xmin, ymin), (xmax, ymax), color, 1)
+    
+    cv2.circle(original_image_np, centers[counter][0:2], 8, color, -1)
+    
+    # Make adjustments to make the label visible for all objects
+    y = ymin - 7 if ymin - 7 > 7 else ymin + 7
+    label = "{}: {:.0f}%".format(classes[class_id], obj['score'] * 100)
+
+    label = "{}: {:.0f}%".format('col', obj['score'] * 100)
+    
+    cv2.putText(original_image_np, label, (xmin, y), 
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+    counter +=1
+    if counter >= max_boxes:
+        break
+
+  # Return the final image
+  original_uint8 = original_image_np.astype(np.uint8)
+  return original_uint8, centers
+
+
+def run_od(TEMP_FILE, DETECTION_THRESHOLD, max_boxes, keyboard):
+    DETECTION_THRESHOLD = 0.10 #@param {type:"number"}  # <<<<<<<<<<<==================
+    filter_boolean=True
+
+    #TEMP_FILE = 'images-marburg/test_images/image3.jpg'
+    #TEMP_FILE = 'carotene-01-800-600.jpg'
+    #TEMP_FILE = 'carotene-01-600-600.jpg'
+    #TEMP_FILE ='images-test/434.jpg'
+
+
+    im = Image.open(TEMP_FILE)
+
+    # Load the TFLite model
+    interpreter = Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+
+    # Run inference and draw detection result on the local copy of the original file
+    detection_result_image, centers = run_odt_and_draw_results(
+        TEMP_FILE,
+        interpreter,
+        filter_boolean,
+        threshold=DETECTION_THRESHOLD,
+        max_boxes=max_boxes
+    )
+
+    # Show the detection result
+    im_boxed=Image.fromarray(detection_result_image)
+
+    new_ext = ("-thresh=%0.2f.png" % DETECTION_THRESHOLD)
+
+    fname=TEMP_FILE.replace(".jpg", new_ext)
+    im_boxed.save(fname, 'PNG')
+    
+    print("Hit any key to close detected colonies window.")
+    im_boxed = cv2.imread(fname)
+    winname = "Detected Colonies"
+    cv2.namedWindow(winname)        # Create a named window
+    im=cv2.resize(im_boxed, (412,412))
+    cv2.moveWindow(winname, 432,20)  # Move it to (40,30)
+    cv2.imshow(winname, im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+
+    #keyboard.processKeyEvent() # consume that key event
+    
+    print("Run colony picker? y=yes, Enter key=no:")
+    keyVal = keyboard.processKeyEvent()
+    
+    if keyVal == ord('y'):
+        print("Running colony picker (wait for robot to finish).")
+        fname_csv=fname.replace(".png", ".csv")
+        header = "cX,cY,class\n"
+        
+        with open(fname_csv, 'w') as f:
+            f.write(header)
+            for c in centers:
+                line = str(c[0]) + "," + str(c[1]) + "," + c[2]
+                f.write(line)
+                f.write("\n")
+                
+        # now save a temporary copy to be sent to the Opentrons robot
+        with open("colony_centers.csv", 'w') as f:   # current set of centers
+            f.write(header)
+            for c in centers:
+                line = str(c[0]) + "," + str(c[1]) + "," + c[2]
+                f.write(line)
+                f.write("\n")
+                print(line)
+    else:
+        print("\nNot running colony picker.\n")
+        
+
+
+
+
+
